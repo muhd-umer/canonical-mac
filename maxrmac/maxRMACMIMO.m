a3c9a2fe93f34e98bd39c9aee0a5422ce188d29e
muhd-umer <thisismumer@gmail.com>
2026-01-22 01:39:55 -0800

    [minor] Apply real func to Rxxs

--- maxrmac/maxRMACMIMO.m
+++ maxrmac/maxRMACMIMO.m
@@ -91,6 +91,8 @@ function [Rxxs, Eun, w, bun] = maxRMACMIMO(H, Lxu, Eu, theta, cb, use_mex)
             Rxxs = Rxx_cell;
         end
 
+        Rxxs = cellfun(@real, Rxxs);
+
         return;
     end
 
